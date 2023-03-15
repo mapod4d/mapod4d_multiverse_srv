e0031dcf2773f33d337bee0376d361e89c577147
Roberto Taglioretti <roberto@taglioretti.it>
2023-03-15 22:36:40 +0100

    Update admin.py

--- mapod4d_multiverse_srv/front/admin.py
+++ mapod4d_multiverse_srv/front/admin.py
@@ -11,6 +11,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # roberto@taglioretti.it
+
 from django.contrib import admin
 
 # Register your models here.
