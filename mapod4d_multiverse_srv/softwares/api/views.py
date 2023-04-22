b7845caa815face4f7359546b731416830e3541d
totrebot <roberto@taglioretti.it>
2023-04-22 22:28:00 +0200

    softwareversion debug

--- mapod4d_multiverse_srv/softwares/api/views.py
+++ mapod4d_multiverse_srv/softwares/api/views.py
@@ -1,6 +1,7 @@
 from softwares.models import Software
 from .serializers import SoftwareSerializer
 from rest_framework import generics
+from django.http import Http404
 
 
 
@@ -13,4 +14,28 @@ class SoftwareDetailView(generics.RetrieveAPIView):
     lookup_field='name'
     queryset = Software.objects.all()
     serializer_class = SoftwareSerializer
+
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+
+        assert lookup_url_kwarg in self.kwargs, (
+            'Expected view %s to be called with a URL keyword argument '
+            'named "%s". Fix your URL conf, or set the `.lookup_field` '
+            'attribute on the view correctly.' %
+            (self.__class__.__name__, lookup_url_kwarg)
+        )
+
+        filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg]}
+        queryset = queryset.filter(**filter_kwargs)
+
+        queryset = queryset.order_by('v1', 'v2', 'v3', 'v4')
+
+        obj = queryset.last()
+
+        if obj is None:
+            raise Http404
+        self.check_object_permissions(self.request, obj)
+
+        return obj
 
